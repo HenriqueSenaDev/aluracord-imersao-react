b49aa7e97c97dd27f74dd038346bf529f7591f38
HenriqueSenaDev <luizteste656@gmail.com>
2022-03-06 14:48:11 -0300

    aula 04

--- pages/chat.js
+++ pages/chat.js
@@ -1,10 +1,27 @@
 import { Box, Text, TextField, Image, Button } from "@skynexui/components";
 import React from "react";
 import appConfig from "../config.json";
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhzZndya2Zmc21sZnBsZ21ybnVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDY1Nzc2NDgsImV4cCI6MTk2MjE1MzY0OH0.-dkN1ULYo-n_SsE3h3NnlVlfEt-ffE7jB04AJAmOnX0";
+const SUPABASE_URL = "https://hsfwrkffsmlfplgmrnug.supabase.co";
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export default function ChatPage() {
   const [mensagem, setMensagem] = React.useState("");
   const [listaDeMensagens, setListaDeMensagens] = React.useState([]);
+
+  React.useEffect(() => {
+    supabaseClient
+      .from("mensagens")
+      .select("*")
+      .order("id", { ascending: false })
+      .then(({ data }) => {
+        console.log("Dados do banco: ", data);
+        setListaDeMensagens(data);
+      });
+  }, []);
 
   /*
   // Usuário
@@ -17,14 +34,21 @@ export default function ChatPage() {
   - [X] Vamos usar o onChange usa o useState (ter if pra caso seja enter pra limpar a variavel)
   - [X] Lista de mensagens 
   */
+
   function handleNovaMensagem(novaMensagem) {
     const mensagem = {
-      id: listaDeMensagens.length + 1,
       de: "vanessametonini",
       texto: novaMensagem,
     };
 
-    setListaDeMensagens([mensagem, ...listaDeMensagens]);
+    supabaseClient
+      .from("mensagens")
+      .insert([mensagem])
+      .then(({ data }) => {
+        console.log(data);
+        setListaDeMensagens([data[0], ...listaDeMensagens]);
+      });
+
     setMensagem("");
   }
 
@@ -70,13 +94,7 @@ export default function ChatPage() {
           }}
         >
           <MessageList mensagens={listaDeMensagens} />
-          {/* {listaDeMensagens.map((mensagemAtual) => {
-                      return (
-                          <li key={mensagemAtual.id}>
-                              {mensagemAtual.de}: {mensagemAtual.texto}
-                          </li>
-                      )
-                  })} */}
+
           <Box
             as="form"
             styleSheet={{
@@ -141,7 +159,7 @@ function Header() {
 }
 
 function MessageList(props) {
-  console.log(props);
+  // console.log(props);
   return (
     <Box
       tag="ul"
@@ -181,7 +199,7 @@ function MessageList(props) {
                   display: "inline-block",
                   marginRight: "8px",
                 }}
-                src={`https://github.com/vanessametonini.png`}
+                src={`https://github.com/${mensagem.de}.png`}
               />
               <Text tag="strong">{mensagem.de}</Text>
               <Text
